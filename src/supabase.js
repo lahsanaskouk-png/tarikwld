e318164c66bee02f00f6cdef9139da3b7b7813fd
lahsanaskouk-png <lahsanaskouk@gmail.com>
2025-12-27 22:26:57 +0100

    Create supabase.js

---
+++ src/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient(
+  'https://whoqdvbglanpkhvtqqqn.supabase.co',
+  'sb_publishable_ij1OSCYAXC7G3htSeCm_Ng_aI4g065z'
+)
